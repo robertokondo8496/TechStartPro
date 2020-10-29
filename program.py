9cb23fb86415d4ccfb2498880898b0ec6983bc51
Roberto Kondo <robertokondo8496@gmail.com>
2020-10-28 23:30:45 -0300

    Implementa parte de update e delete no program.py

--- program.py
+++ program.py
@@ -2,9 +2,11 @@ import reader
 import writedb
 import insert
 import show
+import update
 import drop
 
 fields = ["Name", "Description", "Value", "Categories"]
+filter = {}
 
 while(1):
     print('''Options:
@@ -14,15 +16,64 @@ while(1):
     [3] - Delete Product
     [4] - Exit''')
     opt = input("What option would you like to do:")
+
     if(int(opt) == 0):
-        print("selecionou criar")
+        print("CREATE")
         insert.create()
+
     elif(int(opt) == 1):
-        print("selecionou ler")
+        print("READ")
+        numberFilters = input("Insert how many filters are you going to use (1-4): ")
+        if(numberFilters == ""):
+            numberFilters = 0
+        for i in range(int(numberFilters)):
+            print(fields[i])
+            value = input()
+            if(value != ""):
+                filter.update({fields[i]: value})
+        print(filter)
+        show.show('products', filter)
+        filter.clear() 
+
     elif(int(opt) == 2):
-        print("selecionou editar")
+        print('''UPDATE
+        
+        Please insert what product would you like to change:
+        (You can insert one or more filters)
+
+        ''')
+        newdict={}
+
+        for element in fields:
+            print(element)
+            newelement = input()
+            if(newelement != ""):
+                filter.update({element: newelement})
+        print('''
+        
+        Please insert now new data to write to selected product:
+
+        ''')
+
+        for element in fields:
+            print(element)
+            newelement = input()
+            if(newelement != ""):
+                newdict.update({element: newelement})
+        update.update('products', filter, newdict)
+        filter.clear()
+
     elif(int(opt) == 3):
-        print("selecionou excluir")
+        print("DELETE")
+        
+        for element in fields:
+            print(element)
+            newelement = input()
+            if(newelement != ""):
+                filter.update({element: newelement})
+        drop.delete('products', filter)
+        filter.clear()
+
     elif(int(opt) == 4):
         print("bye")
         break
